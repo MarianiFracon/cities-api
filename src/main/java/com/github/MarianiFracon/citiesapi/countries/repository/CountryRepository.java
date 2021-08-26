aad1ee6da75bbeb8b833469d871760b9942b795d
Mariani Fraçon <88113715+MarianiFracon@users.noreply.github.com>
2021-08-26 16:02:07 -0300

    Revert "Primeiro commit"
    This reverts commit 8fdfb260e63234799309ff5a0e1a7a88e5b4f2d5.

--- src/main/java/com/github/MarianiFracon/citiesapi/countries/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.MarianiFracon.citiesapi.countries.repository;
-
-import com.github.MarianiFracon.citiesapi.countries.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
